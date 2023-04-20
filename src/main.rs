760d07aa1c217215818d0fc0383187fb741e8ec7
Lacey <77512119+CommunismGoose@users.noreply.github.com>
2023-04-20 12:10:08 -0500

    made it more readable
    made my code more readable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use std::io;
 fn main() {
     println!("please choose a number for x");
-    let mut x = String::new();
-    io::stdin()
-        .read_line(&mut x)
-        .expect("Your operating system does not support this");
-    let x: f64 = match x.trim().parse(){
+    let x: f64 = match readline().trim().parse(){
         Ok(num) => num,
         Err(_) => 1.0,
     };
@@ -15,20 +11,13 @@ fn main() {
     println!("x or * for mutiplication");
     println!("/ for division");
     println!("** to raise x to the power of y");
-    let mut opp = String::new();
-    io::stdin()
-        .read_line(&mut opp)
-        .expect("Your operating system does not support this");
-    let opp = opp.trim();
+    let binding = readline();
+    let opp = binding.trim();
     println!("please choose your second number, y");
     if opp == "**"{
-        println!("please choose a number 1 or greater that is an interger")
+        println!("please choose a number 1 or greater that is an integer")
     }
-    let mut y = String::new();
-    io::stdin()
-        .read_line(&mut y)
-        .expect("Your operating system does not support this");
-    let y: f64 = match y.trim().parse(){
+    let y: f64 = match readline().trim().parse(){
         Ok(num) => num,
         Err(_) => 1.0,
     };
@@ -51,19 +40,19 @@ fn main() {
 }
 fn add(x:f64, y:f64) -> f64 {
     let finalnum:f64 = x+y;
-    finalnum
+    return finalnum
 }
 fn sub(x:f64, y:f64) -> f64 {
     let finalnum:f64 = x-y;
-    finalnum
+    return finalnum
 }
 fn multipy(x:f64, y:f64) -> f64 {
     let finalnum:f64 = x*y;
-    finalnum
+    return finalnum
 }
 fn divide(x:f64, y:f64) -> f64 {
     let finalnum:f64 = x/y;
-    finalnum
+    return finalnum
 }
 fn expoent(x:f64, y:f64) -> f64 {
     let mut count: f64 = 1.0;
@@ -72,5 +61,13 @@ fn expoent(x:f64, y:f64) -> f64 {
         finalnum = finalnum*x;
         count=count+1.0
     }
-    finalnum
+    return finalnum
+}
+fn readline()->String{
+    let mut x = String::new();
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Your operating system does not support this");
+    let x = x.trim();
+    return x.to_string()
 }
